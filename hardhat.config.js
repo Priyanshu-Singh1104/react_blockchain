01dc04b9c82fa5741fa1a2df5b119cd210a2e980
Priyanshu SIngh <priyanshu.singh@enpointe.io>
2024-06-30 23:19:16 +0530

    first commit

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,19 @@
+const { network } = require("hardhat");
+
 require("@nomicfoundation/hardhat-toolbox");
 
+require("dotenv").cofig();
 /** @type import('hardhat/config').HardhatUserConfig */
+
+const GOERLI_URL = process.env.GOERLI_URL;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+
 module.exports = {
   solidity: "0.8.24",
+  networks:{
+    goerli:{
+      url:GOERLI_URL,
+      accounts:[PRIVATE_KEY],
+    }
+  }
 };
